cb6f64c65163e4c21fa8d454c38c199b57bcdd5f
“CristhTejada” <cristh.tejada@gmail.com>
2022-11-28 14:45:28 -0600

    Fixed minor typing errors

--- contracts/TCoinERC20.sol
+++ contracts/TCoinERC20.sol
@@ -1,40 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
+pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract TCoin is ERC20 {
-    mapping(address => uint256) public balances;
-    mapping(address => mapping( address => uint256 )) public allowed;
-    event Transfer(address _owner, uint256 _amount);
 
-    constructor() ERC20("TCoin", "TC") {
-    _mint(msg.sender, 1000000000000);
-    }
-    function balanceOf(address _user) public view override returns(uint256) {
-        return balances[_user];
-    }
-    function mintTCoin() public {
-        require(1000 <= balances[msg.sender], "Balance is insufficient for minting the token.");
-        balances[msg.sender] = balances[msg.sender] + 1000;
-
-        emit Transfer(msg.sender, 1000)
-    }
-    function transfer(address _receiver, uint256 _amount) public overrride returns(bool) {
-        require(0 == _amount, "Amount of tokens can't be equal zero");
-        require(_amount > balances[msg.sender], "You need tokens to send");
-
-        balances[msg.sender] = balances[msg.sender] - _amount;
-        blances[_receiver] = balnces[_receiver] + _amount;
-
-        emit Transfer(_receiver, _amount);
-        return true;
-    }
-    function approve(address _delegate, uint256 _amount) public override returns(bool) {
-        allowed[msg.sender][_delegate] = _amount;
-
-        return true;
-    }
-    function allowance(address _owner, address _delegate) public override view returns(uint256) {
-        return allowed[_owner][_delegate];
+contract TCoinERC20 is ERC20 {
+    constructor(uint256 initialSupply) ERC20("TCoin", "TC") {
+        _mint(msg.sender, initialSupply);
     }
 }
     
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,12 +2,12 @@ const hre = require("hardhat");
 
 async function main() {
 
-  const TCoin = await hre.ethers.getContractFactory("TCoinERC20");
-  console.log('Deploying TCoin...');
+  const TCoinERC20 = await hre.ethers.getContractFactory("TCoinERC20");
+  console.log('Deploying TCoinERC20...');
   const token = await TCoinERC20.deploy('10000000000000000000000');
 
   await token.deployed();
-  console.log("TCoin deployed to:", token.address);
+  console.log("TCoinERC20 deployed to:", token.address);
 }
 
 main()
