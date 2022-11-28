d1edc81c10bb14ad17da460996c6cedb33fc69d4
“CristhTejada” <cristh.tejada@gmail.com>
2022-11-28 14:02:35 -0600

    Contract Transfer/Minting function added

--- contracts/TCoinERC20.sol
+++ contracts/TCoinERC20.sol
@@ -3,12 +3,29 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract TCoin is ERC20 {
     mapping(address => uint256) public balances;
-    constructor() ERC20("TCoin", "TC") {
-    _mint(msg.sender, 1000000000000) ;
-    }
+    event Transfer(address _owner, uint256 _amount);
 
+    constructor() ERC20("TCoin", "TC") {
+    _mint(msg.sender, 1000000000000);
+    }
     function balanceOf(address _user) public view override returns(uint256) {
         return balances[_user];
+    }
+    function mintTCoin() public {
+        require(1000 <= balances[msg.sender], "Balance is insufficient for minting the token.");
+        balances[msg.sender] = balances[msg.sender] + 1000;
+
+        emit Transfer(msg.sender, 1000)
+    }
+    function transfer(address _receiver, uint256 _amount) public overrride returns(bool) {
+        require(0 == _amount, "Amount of tokens can't be equal zero");
+        require(_amount > balances[msg.sender], "You need tokens to send");
+
+        balances[msg.sender] = balances[msg.sender] - _amount;
+        blances[_receiver] = balnces[_receiver] + _amount;
+
+        emit Transfer(_receiver, _amount);
+        return true;
     }
 }
     
