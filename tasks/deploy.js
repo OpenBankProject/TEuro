052714ae833924a28a15a1b420cfdc63b8f42cae
CristhTejada <cristh.tejada@gmail.com>
2023-01-02 11:16:03 -0600

    Tasks has beend added.

---
+++ tasks/deploy.js
@@ -0,0 +1,19 @@
+const { task } = require("hardhat/config");
+
+task("deploy", "Deploys Lock contract")
+  .setAction(async (_, hre) => {
+    const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+    const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+    const lockedAmount = hre.ethers.utils.parseEther("1");
+
+    const Lock = await hre.ethers.getContractFactory("Lock");
+    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+    await lock.deployed();
+
+    console.log(
+      `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    );
+  });
