8b48de5fec8a004343cee76f557eecfc9985eba3
CristhTejada <cristh.tejada@gmail.com>
2023-02-26 18:49:07 -0600

    Update model definition with new parameters

--- api-package/src/models.js
+++ api-package/src/models.js
@@ -1,20 +1,56 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const { getEnvKey } = require('./utils.js');
 
-const sequelize = new Sequelize (
-    getEnvKey('DB_URI'), 
-    {
+const sequelize = new Sequelize ({
         dialect: 'sqlite',
-    },
-);
+        storage: "../test.db"
+    });
 
 const User = sequelize.define('User', {
-    username: {
-        type: DataTypes.STRING,
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
         allowNull: false,
-        unique: true
+        primaryKey: true
     },
-    valid: DataTypes.BOOLEAN,
+    username: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        unique: true,
+        defaultValue: DataTypes.UUIDV4
+    },
+    legalName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    dateOfBirth: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    identityDocument: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    identityDocumentCountry: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    issueDate: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    issuePlace: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    expirityDate: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    valid: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
 });
 
 module.exports = {
