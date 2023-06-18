45df8f20e180a59696ac60db2156adfaef5cd34f
CristhTejada <cristh.tejada@gmail.com>
2023-06-18 14:06:26 -0600

    Update contracts licence

--- contract-package/contracts/interfaces/IIdentity.sol
+++ contract-package/contracts/interfaces/IIdentity.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.17;
 
 import { IERC734 } from "./IERC734.sol";
--- contract-package/contracts/modules/requesters/Validator.sol
+++ contract-package/contracts/modules/requesters/Validator.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.17;
 
 import "@api3/airnode-protocol/contracts/rrp/requesters/RrpRequesterV0.sol";
