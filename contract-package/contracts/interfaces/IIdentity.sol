38779979d9aa528e1a45dbfc90f6a4901e07b02a
CristhTejada <cristh.tejada@gmail.com>
2023-07-08 10:31:12 -0600

    Update interfaces with latest changes in Identity contract

--- contract-package/contracts/interfaces/IIdentity.sol
+++ contract-package/contracts/interfaces/IIdentity.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.17;
 
+// Package
 import { IERC734 } from "./IERC734.sol";
 import { IERC735 } from "./IERC735.sol";
 
@@ -10,10 +11,14 @@ import { IERC735 } from "./IERC735.sol";
  *
  * @notice Interface for Identity Contract.
  * Represents the identity of a single user in the system.
- * Contains all the keys and claims for the given user.
+ * Contains all the keys and claims for the given user,
+ * both investors and issuers.
  * @dev This contract is intended to be cloned trough a factory pattern.
  */
-interface IIdentity is IERC734, IERC735 {
+interface IIdentity is
+    IERC734,
+    IERC735
+{
     // ===== UPGRADES =====
     /**
      * @notice Gets the current version of the contract.
